e4ed351b75a1e848415aeafb4927c7315a465709
jimoon <wjdwlans9303@naver.com>
2020-01-31 18:29:22 +0900

    공지사항

--- bitcamp_lms/sql/test.sql
+++ bitcamp_lms/sql/test.sql
@@ -1,7 +1,13 @@
-insert into job_bbs values(job_seq.nextval,10000,1000,'비트캠프','강남',20,'정규직','수시모집','test',1,sysdate);
-insert into job_bbs values(job_seq.nextval,10001,1001,'비트캠프','서초',20,'정규직','수시모집','test',1,sysdate);
-insert into job_bbs values(job_seq.nextval,10002,1002,'비트캠프','종로',20,'정규직','수시모집','test',1,sysdate);
-insert into job_bbs values(job_seq.nextval,10003,1003,'비트캠프','송파',20,'정규직','수시모집','test',1,sysdate);
-insert into job_bbs values(job_seq.nextval,10004,1004,'비트캠프','분당',20,'정규직','수시모집','test',1,sysdate);
-commit;
+insert into job_bbs values(job_seq.nextval,10000,1000,'비트캠프','강남',20,'정규직','수시모집','test1',0,sysdate);
+insert into job_bbs values(job_seq.nextval,10001,1001,'비트캠프','서초',20,'정규직','수시모집','test2',0,sysdate);
+insert into job_bbs values(job_seq.nextval,10002,1002,'비트캠프','종로',20,'정규직','수시모집','test3',0,sysdate);
+insert into job_bbs values(job_seq.nextval,10003,1003,'비트캠프','송파',20,'정규직','수시모집','test4',0,sysdate);
+insert into job_bbs values(job_seq.nextval,10004,1004,'비트캠프','성남',20,'정규직','수시모집','test5',0,sysdate);
+
+insert into ntc_bbs values(ntc_seq.nextval,10000,1000,' 2019 고용노동부 인증평가 우수훈련기관 선정!','test1',0,sysdate);
+insert into ntc_bbs values(ntc_seq.nextval,10001,1001,' 2019년 서울 유망산업 JOB 페스티벌 참여 모집 안내','test2',0,sysdate);
+insert into ntc_bbs values(ntc_seq.nextval,10002,1002,'[비트캠프 종로센터] 2019 서울시 청년 뉴딜일자리 참여자모집 안내','test3',0,sysdate);
+insert into ntc_bbs values(ntc_seq.nextval,10003,1003,'[비트캠프 서초본원] 2019 혁신성장 청년인재 집중양성 교육생 모집','test4',0,sysdate);
+insert into ntc_bbs values(ntc_seq.nextval,10004,1004,' 뉴딜 서울시 일자리사업 클라우드 컴퓨팅과정 합격자 발표','test5',0,sysdate);
+
 select * from JOB_BBS
