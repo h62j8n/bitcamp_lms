aa985cb1ca1d783652c38fd613dfac1def355cb5
94jinwoo <zuz1002@naver.com>
2020-01-31 16:37:29 +0900

    delete test

--- bitcamp_lms/bin/sql/test.sql
+++
@@ -1,7 +0,0 @@
-insert into job_bbs values(job_seq.nextval,10000,1000,'비트캠프','강남',20,'정규직','수시모집','test',1,sysdate);
-insert into job_bbs values(job_seq.nextval,10001,1001,'비트캠프','서초',20,'정규직','수시모집','test',1,sysdate);
-insert into job_bbs values(job_seq.nextval,10002,1002,'비트캠프','종로',20,'정규직','수시모집','test',1,sysdate);
-insert into job_bbs values(job_seq.nextval,10003,1003,'비트캠프','송파',20,'정규직','수시모집','test',1,sysdate);
-insert into job_bbs values(job_seq.nextval,10004,1004,'비트캠프','분당',20,'정규직','수시모집','test',1,sysdate);
-commit;
-select * from JOB_BBS
